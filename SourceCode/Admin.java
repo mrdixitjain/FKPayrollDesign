11bf3ddae5531496d50e0875a58bc1a3611af55f
mrdixitjain <mrdixitjain1999@gmail.com>
2020-05-09 22:56:50 +0530

    Added New File : Admin.java 	this file will contain various operation which can be performed by admin.

---
+++ SourceCode/Admin.java
@@ -0,0 +1,10 @@
+
+import java.util.*;
+
+class Admin {
+    public static enum MethodOfPayment {Paycheck, PaycheckPickup, BankTransfer};
+
+    private static HashMap<String, String> adminDetails;
+
+
+}
