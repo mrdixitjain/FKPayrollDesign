e40dc624e24eb406897a534fbd1d636c02dbfbfb
mrdixitjain <mrdixitjain1999@gmail.com>
2020-05-09 20:28:18 +0530

    New File Added : Generate.java 	contains module to generate random password for first time setup and 	    module to generate random id(Employee id).

---
+++ SourceCode/Generate.java
@@ -0,0 +1,30 @@
+
+package project;
+
+import java.util.*;
+
+class Generate {
+
+	public static String generateId() {
+		Random r = new Random();
+		String id = "";
+		for(int i = 0; i < 4; i++) {
+			id += (char) (r.nextInt(26)+65);
+		}
+		for(int i = 0; i < 6; i++){
+			id += r.nextInt(10);
+		}
+		return id;
+	}
+
+	public static String generatePassword() {
+		Random r = new Random();
+		String password = "";
+		for(int i = 0; i < 10; i++) {
+			password += (char) (r.nextInt(26)+65);
+		}
+		return password;
+	}
+
+
+}
