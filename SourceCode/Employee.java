b4a325bba79fbbdbafea429b63f5188d2390cd3c
mrdixitjain <mrdixitjain1999@gmail.com>
2020-05-09 20:31:35 +0530

    Modified Employee.java 	added password field to it's properties.

--- SourceCode/Employee.java
+++ SourceCode/Employee.java
@@ -6,10 +6,12 @@ class Employee {
     MethodOfPayment mop;
     int id;
     String name;
+    String password;
 
     public Employee(int id, String name, MethodOfPayment mop) {
         this.mop = mop;
         this.id = id;
         this.name = name;
+        password = Generate.generatePassword();
     }
 }
