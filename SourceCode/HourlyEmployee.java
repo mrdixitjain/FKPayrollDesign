eeac37d4e04249a5b330b5e298bda1edad5ba095
mrdixitjain <mrdixitjain1999@gmail.com>
2020-05-10 15:52:10 +0530

    Modified HourlyEmployee.java 	added feature to show details, update password, join union, 	leave union, update method of payment and logout.

--- SourceCode/HourlyEmployee.java
+++ SourceCode/HourlyEmployee.java
@@ -7,13 +7,55 @@ class HourlyEmployee extends Employee {
     private float hourRate;
 
     // called while adding new employee
-    public MonthlyEmployee(String name, String id, String password, String mop, boolean isInUnion, float commissionRate, float hourRate) {
+    public HourlyEmployee(String name, String id, String password, String mop, boolean isInUnion, float commissionRate, float hourRate) {
         super(name, id, password, mop, isInUnion, commissionRate);
         this.hourRate = hourRate;
     }
 
-    public float getHourRate() {
-        return hourRate;
+    public void getDetails() {
+        super.getDetails();
+        System.out.println("Hour Rate : " + hourRate);
+
+    }  
+
+    public void hourlyEmployeeMain() {
+
+        int n = 6;
+        Choice:
+        while(true) {
+            n = showChoices();
+
+            switch(n){
+                case 1 :
+                    updatePassword();
+                    break;
+
+                case 2 : 
+                    joinUnion();
+                    break;
+
+                case 3 : 
+                    leaveUnion();
+                    break;
+
+                case 4 : 
+                    updateMOP();
+                    break;
+
+                case 5 : 
+                    getDetails();
+                    break;
+
+                case 6 :
+                    break Choice;
+
+                default :
+                    System.out.println("\nPlease! enter a valid choice\n");
+                    continue;
+            }
+        }
+
+        System.out.println("\nLogged Out SuccessFully\n");
     }
 
 
